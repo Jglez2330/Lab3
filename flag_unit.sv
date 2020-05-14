80ef64dc53c07979020444e369035623866403eb
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2020-05-14 10:36:11 -0600

    Flag V logic

--- flag_unit.sv
+++ flag_unit.sv
@@ -1,11 +1,14 @@
 // Módulo "Top Level" de la lógica de las banderas
 
 module flag_unit #(parameter N = 2)
-						(input logic[N-1:0] y, output logic z);
+						(input logic[N-1:0] y,
+						input logic so2,so1,yn1,an1,bn1,
+						output logic z,v);
 					
 					
 			flagZero_logic FZL(.y(y), .z(z));
 			flagZero_logic_TestBench FZLTB();
+			flagV_logic FVL(.so2(so2), .so1(so1), .yn1(yn1), .an1(an1), .bn1(bn1), .V(v));
 					
 					
 					
---
+++ xnor_2.sv
@@ -0,0 +1,10 @@
+//Módulo XNOR de 2 bits
+
+
+module xnor_2 (input logic an1,bn1, output logic K);
+
+
+			assign J = ~(an1 ^ bn1);
+			
+			
+endmodule
