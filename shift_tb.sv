8fd6d28675c08ca996a66c0d882edf52d19d2ce2
thechancher <thechancher@gmail.com>
2020-05-16 17:01:19 -0600

    shift arithmetic

--- shift_tb.sv
+++ shift_tb.sv
@@ -5,27 +5,24 @@ module shift_tb;
 
 	parameter bus = 4;
 
-	logic [bus-1:0] a;
+	logic signed [bus-1:0] a;
 	logic [bus-1:0] b;
 	
-	logic [bus-1:0] y;
+	logic signed [bus-1:0] y;
 
 	// shift_right DUT (.a(a), .b(b), .y(y));
-	shift_left DUT (.a(a), .b(b), .y(y));
+	// shift_left DUT (.a(a), .b(b), .y(y));
+	// shift_left_arithmetic DUT (.a(a), .b(b), .y(y));
+	shift_right_arithmetic DUT (.a(a), .b(b), .y(y));
 
 	initial begin
-		a = {bus{1'b1}};
-		b = {bus{1'b0}};
-	end
+		a = 4'b1001;
 
-	initial begin
-    	b = 4'b0000; 
-    	#period; $assertoff(b,4'b1111);
+    	b = 4'b0000; #period;
     	b = 4'b0001; #period;
     	b = 4'b0010; #period;
     	b = 4'b0011; #period;
 		b = 4'b0100; #period;
-		b = 4'b0101; #period;
 	end  
 
-endmodule  
+endmodule     
